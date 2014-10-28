9522c8eebcf64ab21bb7c46e406aa830ab142482
Fabien SK <fab@fabsk.eu>
2014-10-28 22:16:02 +0100

    Results in a tab and links

--- data/result.js
+++ data/result.js
@@ -1,29 +1,44 @@
 
-function add_td(tr, text) {
+// add a cell. Parameters:
+// tr: parent
+// value: either a text or a link info
+// is_link: true if link
+function add_td(tr, value, is_link) {
     var td = document.createElement("td");        
-    td.appendChild(document.createTextNode(text));
+    if (is_link==false) {
+        td.appendChild(document.createTextNode(value));
+    } else {
+        for(cnt in value) {
+            if (cnt>0) {
+                td.appendChild(document.createTextNode(" "));
+            }
+            var a = document.createElement("a");
+            a.setAttribute("href", value[cnt][0]);
+            a.appendChild(document.createTextNode(value[cnt][1]));
+            td.appendChild(a);
+        }
+    }
     tr.appendChild(td);
 }
 
+// add a line in the result table
 function add_tr(table, items) {
     var tr = document.createElement("tr");
     for(cnt in items) {
-        add_td(tr, items[cnt]);
+        add_td(tr, items[cnt], cnt>=3);
     }
     table.appendChild(tr);
 }
 
 // init the result window
-self.on('message', function(param) {
-    var result = document.getElementById("result");
+self.port.on('message', function(param) {
+    var tbody = document.getElementById("result");
     param.sort(function(a, b) { return a[1]-b[1];})
-    var table = document.createElement("table");
-    var thead = document.createElement("thead");
-    table.appendChild(thead);
-    var tbody = document.createElement("tbody");
-    table.appendChild(tbody);
-    add_tr(thead, ["Kanji", "Level", "Count"])
     for(cnt in param) {
+        var links = new Array();
+        links.push(["http://kanji.free.fr/kanji.php?utf8="+param[cnt][0], "Fr"]);
+        links.push(["http://jisho.org/kanji/details/"+param[cnt][0], "En"]);
+        param[cnt].push(links);
         add_tr(tbody, param[cnt])
     }
     result.appendChild(table);
